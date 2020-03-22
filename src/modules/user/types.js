9641e71f19954136ba89d5da6148ae81ad43311c
Raphael Jorge <raphaeljsn1@gmail.com>
2020-03-22 15:28:02 -0300

    add user types

---
+++ src/modules/user/types.js
@@ -0,0 +1,31 @@
+// Imports
+import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql'
+
+// User type
+const UserType = new GraphQLObjectType({
+  name: 'user',
+  description: 'User type',
+
+  fields: () => ({
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+    role: { type: GraphQLString },
+    createdAt: { type: GraphQLString },
+    updatedAt: { type: GraphQLString }
+  })
+})
+
+// User Login type
+const UserLoginType = new GraphQLObjectType({
+  name: 'userAuth',
+  description: 'User Authentication Type',
+
+  fields: () => ({
+    user: { type: UserType },
+    token: { type: GraphQLString }
+  })
+})
+
+export { UserType, UserLoginType }
