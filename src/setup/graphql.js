2cacbe3b60dc4de851dd7b10f2cebc036c61eb23
Raphael Jorge <raphaeljsn1@gmail.com>
2020-03-22 15:23:39 -0300

    Upload setup

---
+++ src/setup/graphql.js
@@ -0,0 +1,31 @@
+// Imports
+import { ApolloServer } from 'apollo-server-express'
+
+// App Imports
+import serverConfig from '../config/server.json'
+import authentication from './authentication'
+import schema from './schema'
+
+// Setup GraphQL
+export default function (server) {
+  console.info('SETUP - GraphQL...')
+
+  server.use(authentication)
+
+  const graphqlServer = new ApolloServer({ 
+    schema, 
+    context: ({ req }) => {
+      return { 
+         auth: {
+           user: req.user,
+           isAuthenticated: req.user && req.user.id > 0
+         }
+       }
+     },
+    debug: false
+  })
+  graphqlServer.applyMiddleware({ 
+    app: server, 
+    path: '/'
+  })
+}
---
+++ src/setup/models.js
@@ -0,0 +1,20 @@
+// Imports
+import Sequelize from 'sequelize'
+
+// App Imports
+import databaseConnection from './database'
+
+const models = {
+  User: databaseConnection.import('../modules/user/model'),
+}
+
+Object.keys(models).forEach(modelName => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models)
+  }
+})
+
+models.sequelize = databaseConnection
+models.Sequelize = Sequelize
+
+export default models
