0ff84ba0a7976b7635467402180f5022d3bb537b
Raphael Jorge <raphaeljsn1@gmail.com>
2020-03-22 15:32:16 -0300

    change filename and fix bugs

--- src/setup/load-modules.js
+++ src/setup/load-modules.js
@@ -1,21 +1,33 @@
 // Imports
-import jwt from 'jsonwebtoken'
-import serverConfig from '../config/server.json'
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 
-// Authentication middleware
-export default function (request, response, next) {
-  let authToken = request.headers.authorization
+// App Imports
+import { NODE_ENV } from '../config/env'
 
-  if (authToken && authToken !== null) {
-    try {
-      const token = authToken.split(' ')
-      request.user = jwt.verify(token[1], serverConfig.secret)
-    } catch (e) {
-      console.warn('Invalid token detected.')
-    }
-  } else {
-    request.user = {}
+// Load express modules
+export default function (server) {
+  console.info('SETUP - Loading modules...')
+
+  // Enable CORS
+  server.use(cors())
+
+  // Request body parser
+  server.use(bodyParser.json())
+  server.use(bodyParser.urlencoded({ extended: false }))
+
+  // Request body cookie parser
+  server.use(cookieParser())
+
+  // Static files folder
+  server.use(express.static(path.join(__dirname, '..', '..', 'public')))
+
+  // HTTP logger
+  if(NODE_ENV === 'development') {
+    server.use(morgan('tiny'))
   }
-  //console.log(request.user);
-  next()
 }
--- src/setup/start-server.js
+++ src/setup/start-server.js
@@ -1,33 +1,15 @@
-// Imports
-import express from 'express'
-import path from 'path'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-
 // App Imports
-import { NODE_ENV } from '../config/env'
+import { PORT, NODE_ENV } from '../config/env'
 
-// Load express modules
+// Start server
 export default function (server) {
-  console.info('SETUP - Loading modules...')
+  console.info('SETUP - Starting server..')
 
-  // Enable CORS
-  server.use(cors())
-
-  // Request body parser
-  server.use(bodyParser.json())
-  server.use(bodyParser.urlencoded({ extended: false }))
-
-  // Request body cookie parser
-  server.use(cookieParser())
-
-  // Static files folder
-  server.use(express.static(path.join(__dirname, '..', '..', 'public')))
-
-  // HTTP logger
-  if(NODE_ENV === 'development') {
-    server.use(morgan('tiny'))
-  }
+  server.listen(PORT, (error) => {
+    if (error) {
+      console.error('ERROR - Unable to start server.')
+    } else {
+      console.info(`INFO - Server started on http://localhost:${ PORT } [${ NODE_ENV }]`)
+    }
+  })
 }
